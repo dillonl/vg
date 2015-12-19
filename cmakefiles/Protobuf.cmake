a58c6b8aa2386c1b603d7cb53b897d7ad37a65dc
dillonl <dlee@genetics.utah.edu>
2015-12-18 21:29:05 -0700

    adding more cmake magic

--- cmakefiles/Protobuf.cmake
+++ cmakefiles/Protobuf.cmake
@@ -25,37 +25,39 @@
 #  DEALINGS IN THE SOFTWARE.
 
 # Setting up external library protobuf
-
-SET(PROTOBUF_PROJECT protobuf_project CACHE INTERNAL "protobuf project name")
-SET(PROTOBUF_DIR ${CMAKE_BINARY_DIR}/externals/protobuf CACHE INTERNAL "protobuf project directory")
-SET(PROTOBUF_LIB)
-ExternalProject_Add(${PROTOBUF_PROJECT}
-	GIT_REPOSITORY https://github.com/google/protobuf.git
-	GIT_TAG "master"
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-	BINARY_DIR ${PROTUBUF_DIR}
-	SOURCE_DIR ${PROTOBUF_DIR}
-    CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure
-    BUILD_COMMAND "make"  -j4
-    BUILD_IN_SOURCE 1
-    INSTALL_COMMAND  ""
-	CMAKE_CACHE_ARGS
+include(FindProtobuf)
+#IF(NOT ${PROTOBUF_FOUND})
+    SET(PROTOBUF_PROJECT protobuf_project CACHE INTERNAL "protobuf project name")
+    SET(PROTOBUF_DIR ${CMAKE_BINARY_DIR}/externals/protobuf CACHE INTERNAL "protobuf project directory")
+    SET(PROTOBUF_LIB)
+    ExternalProject_Add(${PROTOBUF_PROJECT}
+        GIT_REPOSITORY https://github.com/google/protobuf.git
+        GIT_TAG "master"
+        UPDATE_COMMAND ""
+        PATCH_COMMAND ""
+        BINARY_DIR ${PROTUBUF_DIR}
+        SOURCE_DIR ${PROTOBUF_DIR}
+        CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure
+        BUILD_COMMAND "make" -j4
+        BUILD_IN_SOURCE 1
+        INSTALL_COMMAND  ""
+        CMAKE_CACHE_ARGS
         -DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}
         -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
-)
+    )
+    
+    ExternalProject_Get_Property(${PROTOBUF_PROJECT} INSTALL_DIR)
+    ExternalProject_Get_Property(${PROTOBUF_PROJECT} SOURCE_DIR)
+    ExternalProject_Get_Property(${PROTOBUF_PROJECT} BINARY_DIR)
+    
+    LINK_DIRECTORIES(${SOURCE_DIR}/src/.libs/)
+    SET(PROTOBUF_LIBRARY ${SOURCE_DIR}/src/.libs/libprotobuf.a CACHE INTERNAL "ProtoBuf Lib")
+    SET(PROTOBUF_INCLUDE_DIR ${SOURCE_DIR}/src/ CACHE INTERNAL "ProtoBuf Include")
+	SET(PROTOBUF_PROTOC_EXECUTABLE ${SOURCE_DIR}/src/protoc CACHE INTERNAL "ProtoBuf Executable")
+    message("ProtobufInclude ${PROTOBUF_INCLUDE_DIR}")
 
-ExternalProject_Get_Property(${PROTOBUF_PROJECT} INSTALL_DIR)
-ExternalProject_Get_Property(${PROTOBUF_PROJECT} SOURCE_DIR)
-ExternalProject_Get_Property(${PROTOBUF_PROJECT} BINARY_DIR)
+	message("asdf: ${PROTOBUF_PROTOC_EXECUTABLE} ${SOURCE_DIR}")
 
-message("I ${INSTALL_DIR}")
-message("S ${SOURCE_DIR}")
-message("B ${BINARY_DIR}")
-
-#LINK_DIRECTORIES(${BINARY_DIR}/runtime/src/)
-#SET(PROTOBUF_LIB libomp CACHE INTERNAL "ProtoBuf Lib")
-SET(PROTOBUF_INCLUDE_DIRS ${SOURCE_DIR}/src/ CACHE INTERNAL "ProtoBuf Include")
-message("ProtobufInclude ${PROTOBUF_INCLUDE_DIRS}")
+#ENDIF()
 
 
